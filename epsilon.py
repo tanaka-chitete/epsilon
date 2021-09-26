3c10a9554f54e51c7cacb5771b2a92a43dce88b8
Tanaka Chitete <tanaka.chitete@gmail.com>
2021-09-26 10:27:23 +0800

    Implement class for retrieving user input

--- epsilon.py
+++ epsilon.py
@@ -1,4 +1,5 @@
 from translator import get_translations
+from user_interface import getInt, getStr
 from output import print_translations, write_translations
 from textwrap import dedent
 
@@ -21,9 +22,7 @@ def main():
         12. Spanish
         13. Turkish"""
     print(dedent(source_languages_menu))
-    print("Source language number: ", end="")
-    source_number = int(input())
-    # source_number = scan_int("Source language number: ", 1, 13)  # TODO: Implement user_input.py class
+    source_number = getInt(1, 13, "Source language number: ")
 
     print()  # Formatting purposes
 
@@ -44,15 +43,11 @@ def main():
         12. Spanish
         13. Turkish"""
     print(dedent(target_languages_menu))
-    print("Target language number: ", end="")
-    target_number = int(input())
-    # target_number = scan_int("Target language number: ", 0, 13)  # TODO: Implement user_input.py class
+    target_number = getInt(0, 13, "Target language number: ")  # TODO: Implement user_input.py class
 
     print()  # Formatting purposes
 
-    print("Word to translate: ", end="")
-    word_to_translate = input()
-    # word_to_translate = scan_string("Word to translate: ")
+    word_to_translate = getStr("Word to translate: ")
 
     print("\nTranslating...\n")
 
